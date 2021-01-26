3dcc1679bdbbf39c93996145ab9b257c01c3c6ee
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-26 03:34:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,11 @@
 require 'pry'
 
 class Artist
-  attr_accessor :name
-  attr_reader :songs
-  
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
-
+  
+  attr_accessor :name
+  attr_reader :songs
 
   @@artists = []
 
